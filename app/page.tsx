f0e7f1607f6a1053a1fc855e86c0a4f43b5fd7f9
Danzrrr <graita.sukma@gmail.com>
2024-06-11 08:45:13 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const meong = "";
+
+  const toBeMeong = () => {
+    const meong = "meong";
+    console.log(meong);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
