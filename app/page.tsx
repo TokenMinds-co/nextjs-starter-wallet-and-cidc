1d513bcb4a71bd1c48b7fea7c0798d0f23fc919b
Danzrrr <graita.sukma@gmail.com>
2024-06-12 16:18:06 +0700

    error 1

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            B'y{" "}
+            B'y {" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -50,7 +50,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
+            Doc's hello therer{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
