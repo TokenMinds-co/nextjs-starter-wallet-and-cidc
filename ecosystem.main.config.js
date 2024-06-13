3edf770b2b701e16f1e491219c6a0fef1ac52367
Danzrrr <graita.sukma@gmail.com>
2024-06-13 10:06:51 +0700

    chore: Update lint.yml to include 'main' branch in pull_request event

--- ecosystem.main.config.js
+++ ecosystem.main.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "next-with-gh-actions-main",
       script: "node_modules/next/dist/bin/next", // npm start
-      args: "start",
+      args: "start -p 3001",
       listen_timeout: 12000, // Hack Without timeout we will get downtime in deployment
       instances: "1", // Max Use all the CPU
       exec_mode: "cluster", // Cluster Mode
